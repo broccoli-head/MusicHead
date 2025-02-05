6f5e303afe9c579c9421c5573ca28cba6824c8ef
Łukasz Rudowski <luki.winners1@gmail.com>
2025-02-05 20:56:56 +0100

    Działa strona logowania i rejestracji

--- MusicHead/strona/urls.py
+++ MusicHead/strona/urls.py
@@ -6,4 +6,7 @@ app_name = "strona"
 urlpatterns = [
     path('', views.glowna, name="glowna"),
     path('dodaj/', views.dodaj_piosenke, name="dodaj_piosenke"),
+    path('rejestracja/', views.rejestracja, name='rejestracja'),
+    path('login/', views.zaloguj, name='login'),
+    path('wyloguj/', views.wyloguj, name='wyloguj'),
 ]
--- MusicHead/strona/views.py
+++ MusicHead/strona/views.py
@@ -1,10 +1,51 @@
-from django.shortcuts import render
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
 from .models import Piosenka
 
 def glowna(request):
     return render(request, "strona/glowna.html")
 
+
+def rejestracja(request):
+    if request.method == 'POST':
+        formularz = UserCreationForm(request.POST)
+        if formularz.is_valid():
+            uzytkownik = formularz.save()
+            login(request, uzytkownik)
+            return redirect('/')
+    else:
+        formularz = UserCreationForm()
+
+    return render(request, 'strona/rejestracja.html', {'form': formularz})
+
+
+def zaloguj(request):
+    if request.method == 'POST':
+        formularz = AuthenticationForm(data = request.POST)
+        if formularz.is_valid():
+            uzytkownik = formularz.get_user()
+            login(request, uzytkownik)
+            
+            if not request.POST.get('zapamietaj'):
+                request.session.set_expiry(0)
+            else:
+                request.session.set_expiry(31536000)
+            return redirect('/')
+    else:
+        formularz = AuthenticationForm()
+    return render(request, 'strona/login.html', {'form': formularz})
+
+
+def wyloguj(request):
+    logout(request)
+    return redirect('/')
+
+
 def dodaj_piosenke(request):
+    if not request.user.is_authenticated:
+        return render(request, "strona/niezalogowany.html")
+    
     if request.method == 'POST':
         tytul = request.POST.get("tytul")
         wykonawcy = request.POST.get("wykonawcy")
@@ -12,6 +53,10 @@ def dodaj_piosenke(request):
         if not tytul or not wykonawcy:
             wiadomosc = "Pola tytuł oraz wykonawcy są obowiązkowe!"
             return render(request, "strona/dodaj.html", {"wiadomosc":wiadomosc});
+    
+        if Piosenka.objects.filter(tytul=tytul, wykonawcy=wykonawcy).exists():
+            wiadomosc = f'Piosenka o tytule "{tytul}" artysty {wykonawcy} istnieje już w bazie danych!'
+            return render(request, "strona/dodaj.html", {"wiadomosc":wiadomosc});  
     
         data = request.POST.get("data") if request.POST.get("data") else None   #bez tej linii wyrzuca błąd invalid format data jezeli pole jest puste
 
